b1eabb1bb84c9d3c120587d796e0e79c51904823
joaovictor1102 <94561761+joaovictor1102@users.noreply.github.com>
2022-06-08 15:00:22 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,44 +1,49 @@
-const Engine = Matter.Engine;
-const World= Matter.World;
-const Bodies = Matter.Bodies;
+var fundoImg, fundo;
+var fogueteImg, foguete;
+var meteoro, meteoroImg;
 
-var engine, world;
-var box1;
-var box2;
-var ground;
-var pig1;
-var log1;
+function preload() {
+
+   fundoImg = loadImage("fundo.webp");
+   fogueteImg = loadImage("foguete.png");
+   meteoroImg = loadImage("obstaculo1.png");
 
 
-function setup(){
-    var canvas = createCanvas(1200,400);
-    engine = Engine.create();
-    world = engine.world;
-    ground = new Ground(600,height,1200,20);
-
-    box1 = new Box(200,300,50,50);
-    box2 = new Box(200,100,50,100);
-    pig1 = new Pig(200,200);
-    log1 = new Log(100, 300, 100,PI/2);
-
-    box3 = new Box(700,240,70,70);
-    box4 = new Box(920,240,70,70);
-    pig2 = new Pig(810,220);
-
-    log2 = new Log(810,180,300,PI/2);
 }
 
-function draw(){
-    background(0);
-    Engine.update(engine);
-    box1.display();
-    box2.display();
-    ground.display();
-    pig1.display();
-    log1.display();
-    box3.display();
-    box4.display();
-    pig2.display();
-    log2.display();
-   
+
+function setup() {
+  createCanvas(800,800);
+  fundo = createSprite(400, 400);
+  fundo.addImage(fundoImg);
+  fundo.scale = 1.8;
+  foguete = createSprite(400, 400);
+  foguete.addImage(fogueteImg);
+  foguete.scale = 0.2;
+  meteoro = createSprite(300, 300);
+  meteoro.addImage(meteoroImg);
+  meteoro.scale = 0.1;
+
+  
+}
+
+function draw() {
+  background(fundoImg); 
+  
+  fundo.velocityY = 4;
+
+  if (fundo.y > 800) {
+     fundo.y = 400;
+  }
+
+  foguete.x = mouseX;
+  foguete.y = mouseY;
+
+  edges = createEdgeSprites();
+
+  foguete.collide(edges[3]);
+  
+
+  drawSprites();
+
 }
